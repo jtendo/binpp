d3f45a42ded2bb1745ed1e249409548aa555d76c
Adam Rutkowski <hq@mtod.org>
2016-08-14 22:35:01 +0200

    Update package metadata

--- src/binpp.erl
+++ src/binpp.erl
@@ -1,19 +1,4 @@
-%%%
-%%%           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-%%%                   Version 2, December 2004
-%%%
-%%% Copyright (C) 2011 Adam Rutkowski <hq@mtod.org>
-%%%
-%%% Everyone is permitted to copy and distribute verbatim or modified
-%%% copies of this license document, and changing it is allowed as long
-%%% as the name is changed.
-%%%
-%%%            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-%%%   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-%%%
-
 %% @doc Pretty printer for Erlang binaries.
-%%
 -module(binpp).
 -author('Adam Rutkowski hq@mtod.org').
 
